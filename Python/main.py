995e79a52cf3a647a0bf0f4916e62279f8c8d4a3
Harshp31 <harshpanwar827@gmail.com>
2026-05-03 16:32:19 +0530

    Create main.py

---
+++ Python/main.py
@@ -0,0 +1,91 @@
+# First Program
+#print("Hello, World!") 
+
+# a = 10
+# b = a
+# print(b) ==  10
+
+# variable
+  #strings
+# first_name = "Harsh"
+# food = "Pizza"
+# email = "13harsh@gmail.com"
+
+# print(f"Hello, {first_name}") //String Formatting
+# print(f"Your favorite food is {food}")
+# print(f"Your email is {email}")
+
+# s = "hello"
+# #s[0] = "H"     #Error
+
+# s = "H" + s[1:]
+# print(s)
+
+#Operations in Strings
+  #1. Concatenation 
+# name = "Hello" + " World"
+# print(name)
+
+# first_name = "Harsh"
+# last_name = "Panwar"
+
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+
+ # 2. Repetition
+# name = "Hello" * 3
+# print(name)
+
+# 3. Indexing 
+# s = "Python"
+# s[0] #P
+# s[-1] #n
+
+# credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[4])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:]) 
+# print(credit_number[-4:])
+# print(credit_number[::2]) //step
+
+# last_digits = credit_number[-4:]
+# print(last_digits)
+
+# credit_number = credit_number[::-1]
+# print(f"Reverse of number is {credit_number}")
+
+# # 4. Slicing
+# s = "Python"
+# print(s[0:4])  #Pyth 
+# print(s[2:5])  #tho
+
+# 4- Format Specifiers:- {value:flags} format a value based on what (you want to display) flags are inserted.
+
+
+
+
+#Methods of Strings
+
+# s = "My name is python"
+
+# print(s.upper())
+# print(s.lower())
+# print(s.strip())
+# print(s.replace("Python", "Java"))
+# print(s) #My Name is Python
+
+# print(s.find("Name")) #3
+# print(len(s)) #17
+# print(s.rfind("is")) #8
+# print(s.capitalize()) #My name is python
+# print(s.isdigit()) #False
+# print(s.isalpha()) #False
+# print(s.isalnum()) #False
+# print(s.count(" ")) #3
+
+
+
+
