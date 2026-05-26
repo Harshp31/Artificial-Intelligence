4b184f158cd2d759d36ec7d17721a0b85fd3eec2
Harshp31 <harshpanwar827@gmail.com>
2026-05-26 22:57:06 +0530

    Add patient data JSON file and refactor API to load patient records

--- Backend/Basics/Main.py
+++ Backend/Basics/Main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()     #Creates backend application
 
-@app.get("/")     
-def home ():
-    return {"message": "FastApi is Working"}
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    
+    return data
 
-@app.get("/user/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id}
+@app.get("/")     
+def start():
+    return {'message': 'Patient Management System API'}
+
 
 @app.get('/about')
 def hello():
-    return {"message": "Harsh panwar"}
+    return {"message": "A Fully Functional API to manage your patient records"}
 
-@app.route("/signup")
-def signup():
-    return {"message": "User Created Sucessfully"}
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
